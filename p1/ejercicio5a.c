14e7289d939cbebae3aa18872c33c8c708cec15a
juan260 <superjuan260@hotmail.com>
2017-02-17 16:11:52 +0100

    Hola

---
+++ p1/ejercicio5a.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define NUM_PROC 3
+int main (void)
+{
+	int pid;
+	int i;
+	for (i=0; i < NUM_PROC; i++){
+		if ((pid=fork()) <0 ){
+			printf("Error haciendo fork\n");
+			exit(EXIT_FAILURE);
+		}else if (pid ==0){
+			printf("HIJO %d\n", i);
+		}else{
+			printf ("PADRE %d\n", i);
+		}
+		wait()
+		}
+		exit(EXIT_SUCCESS);
+	}
