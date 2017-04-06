b4c07a0419decaacea018ef565e4b0714512b09a
juan260 <superjuan260@hotmail.com>
2017-04-07 00:12:37 +0200

    asd

--- G2202_P11_3/semaforos.c
+++ G2202_P11_3/semaforos.c
@@ -17,17 +17,17 @@
 Nombre: Inicializar_Semaforo.
 Descripcion: Inicializa los semaforos indicados.
 Entrada:
-	int semid: Identificador del semaforo.
-	unsigned short *array: Valores iniciales.
+    int semid: Identificador del semaforo.
+    unsigned short *array: Valores iniciales.
 Salida:
-	int: OK si todo fue correcto, ERROR en caso de error.
+    int: OK si todo fue correcto, ERROR en caso de error.
 ************************************************************/
 int Inicializar_Semaforo(int semid, unsigned short *array){
     union semun {
         int val;
         struct semid_ds *semstat;
         unsigned short *array;
-    } arg;	
+    } arg;    
     
     arg.array=array;    
     
@@ -35,22 +35,22 @@ int Inicializar_Semaforo(int semid, unsigned short *array){
         perror("Error al inicializar el semaforo\n");
     }
 
-	return OK;
+    return OK;
 }   
 
 /***************************************************************
 Nombre: Borrar_Semaforo.
 Descripcion: Borra un semaforo.
 Entrada:
-	int semid: Identificador del semaforo.
+    int semid: Identificador del semaforo.
 Salida:
-	int: OK si todo fue correcto, ERROR en caso de error.
+    int: OK si todo fue correcto, ERROR en caso de error.
 ***************************************************************/
 int Borrar_Semaforo(int semid){
-	if(semctl(semid,0,IPC_RMID)<0){
-		return ERROR;
-	}
-	return OK;
+    if(semctl(semid,0,IPC_RMID)<0){
+        return ERROR;
+    }
+    return OK;
 }
 
 /***************************************************************
@@ -58,32 +58,32 @@ Nombre: Crear_Semaforo.
 Descripcion: Crea un semaforo con la clave y el tamaño
 especificado. Lo inicializa a 0.
 Entrada:
-	key_t key: Clave precompartida del semaforo.
-	int size: Tamaño del semaforo.
+    key_t key: Clave precompartida del semaforo.
+    int size: Tamaño del semaforo.
 Salida:
-	int *semid: Identificador del semaforo.
-	int: ERROR en caso de error,
-		0 si ha creado el semaforo,
-		1 si ya estaba creado.
+    int *semid: Identificador del semaforo.
+    int: ERROR en caso de error,
+        0 si ha creado el semaforo,
+        1 si ya estaba creado.
 **************************************************************/
 int Crear_Semaforo(key_t key, int size, int *semid){
-	int sem, ret, i;
+    int sem, ret, i;
     unsigned short *array;
-	
+    
     sem = semget(key, size, IPC_CREAT | IPC_EXCL | SHM_R | SHM_W);
-	ret = 0;
-	if(sem==-1){
-		if(errno==EEXIST){
-			sem = semget(key, size, 
-				IPC_CREAT | SHM_R | SHM_W);
-			ret = 1;
-		}
+    ret = 0;
+    if(sem==-1){
+        if(errno==EEXIST){
+            sem = semget(key, size, 
+                IPC_CREAT | SHM_R | SHM_W);
+            ret = 1;
+        }
         if(sem==-1){
-			return ERROR;
-		}
+            return ERROR;
+        }
     }
 
-	array=(unsigned short*)malloc(sizeof(unsigned short)*size);
+    array=(unsigned short*)malloc(sizeof(unsigned short)*size);
     
     if(array==NULL){
         Borrar_Semaforo(sem);
@@ -97,48 +97,48 @@ int Crear_Semaforo(key_t key, int size, int *semid){
     if(Inicializar_Semaforo(sem, array)==ERROR){
         Borrar_Semaforo(sem);
         perror("Error al inicializar el semaforo\n");
-	    return ERROR;
+        return ERROR;
     }
-    *semid=sem;	
-	free(array);
+    *semid=sem;    
+    free(array);
     return ret;
-}	
+}    
 
 /**************************************************************
 Nombre: Down_Semaforo
 Descripcion: Baja el semaforo indicado
 Entrada:
-	int semid: Identificador del semaforo.
-	int num_sem: Semaforo dentro del array.
-	int undo: Flag de modo persistente pese a finalización
-		abrupta.
+    int semid: Identificador del semaforo.
+    int num_sem: Semaforo dentro del array.
+    int undo: Flag de modo persistente pese a finalización
+        abrupta.
 Salida:
-	int: OK si todo fue correcto, ERROR en caso de error.
+    int: OK si todo fue correcto, ERROR en caso de error.
 ***************************************************************/
 int Down_Semaforo(int id, int num_sem, int undo){
-	struct sembuf sem_oper;
-	sem_oper.sem_num = num_sem;
-	sem_oper.sem_op =-1; 
-	sem_oper.sem_flg = undo; 
-	if(semop (id, &sem_oper, 1)==-1){
-  	        return ERROR;
+    struct sembuf sem_oper;
+    sem_oper.sem_num = num_sem;
+    sem_oper.sem_op =-1; 
+    sem_oper.sem_flg = undo; 
+    if(semop (id, &sem_oper, 1)==-1){
+              return ERROR;
         }
         return OK;
 
-	}
+    }
 
 /***************************************************************
 Nombre: DownMultiple_Semaforo
 Descripcion: Baja todos los semaforos del array indicado
-	por active.
+    por active.
 Entrada:
-	int semid: Identificador del semaforo.
-	int size: Numero de semaforos del array.
-	int undo: Flag de modo persistente pese a finalización
-		abrupta.
-	int *active: Semaforos involucrados.
+    int semid: Identificador del semaforo.
+    int size: Numero de semaforos del array.
+    int undo: Flag de modo persistente pese a finalización
+        abrupta.
+    int *active: Semaforos involucrados.
 Salida:
-	int: OK si todo fue correcto, ERROR en caso de error.
+    int: OK si todo fue correcto, ERROR en caso de error.
 ***************************************************************/
 int DownMultiple_Semaforo(int id,int size,int undo,int *active){
     int i;
@@ -153,12 +153,12 @@ int DownMultiple_Semaforo(int id,int size,int undo,int *active){
 Nombre:Up_Semaforo
 Descripcion: Sube el semaforo indicado
 Entrada:
-	int semid: Identificador del semaforo.
-	int num_sem: Semaforo dentro del array.
-	int undo: Flag de modo persistente pese a finalizacion
-		abupta.
+    int semid: Identificador del semaforo.
+    int num_sem: Semaforo dentro del array.
+    int undo: Flag de modo persistente pese a finalizacion
+        abupta.
 Salida:
-	int: OK si todo fue correcto, ERROR en caso de error.
+    int: OK si todo fue correcto, ERROR en caso de error.
 ***************************************************************/
 int Up_Semaforo(int id, int num_sem, int undo){
         struct sembuf sem_oper;
@@ -166,24 +166,24 @@ int Up_Semaforo(int id, int num_sem, int undo){
         sem_oper.sem_op =+1;
         sem_oper.sem_flg = undo;
         if(semop (id, &sem_oper, 1)==-1){
-		return ERROR;
-	}
-	return OK;
+        return ERROR;
+    }
+    return OK;
         
 }
 
 /***************************************************************
 Nombre: UpMultiple_Semaforo
 Descripcion: Sube todos los semaforos del array indicado
-	por active.
+    por active.
 Entrada:
-	int semid: Identificador del semaforo.
-	int size: Numero de semaforos del array.
-	int undo: Flag de modo persistente pese a finalización
-		abrupta.
-	int *active: Semaforos involucrados.
+    int semid: Identificador del semaforo.
+    int size: Numero de semaforos del array.
+    int undo: Flag de modo persistente pese a finalización
+        abrupta.
+    int *active: Semaforos involucrados.
 Salida:
-	int: OK si todo fue correcto, ERROR en caso de error.
+    int: OK si todo fue correcto, ERROR en caso de error.
 ***************************************************************/
 int UpMultiple_Semaforo(int id,int size, int undo, int *active){
     int i;
