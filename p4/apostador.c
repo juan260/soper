d8d17c757346db28ccc80435b1e7d86b0e71d0c8
Carlos <carlosisamar@gmail.com>
2017-05-11 12:16:20 +0200

    apostador

--- p4/apostador.c
+++ p4/apostador.c
@@ -1,14 +1,31 @@
 #include "apostador.h"
 
-void apostador(int numapostadores, int numcaballos, int msqid, int *tiempo){	
+int apostador(int numapostadores, int numcaballos, int msqid, int tiempop){	
 	srand(time(NULL)*getpid()>>16);
-	while (*tiempo>0){
-		ApuestaMsg apuesta;
-		apuesta.mtype = 1;
-		apuesta.numapostador = rand()%numapostadores;
-		apuesta.numcaballo = rand()%numcaballos;
-		apuesta.apuesta = (double)rand()/RAND_MAX*1000;
-		msgsnd(msqid, (struct msgbuf*) &apuesta, sizeof(ApuestaMsg)-sizeof(long), 0);
-		usleep(100000);
+	int ret, i;
+	if((tiempo = shmat(tiempop, NULL, 0))==(void*)-1){
+		perror("Error al obtener la zona compartida de memoria.\n");
+		*tiempo=-1;
+		return -1
+	}
+	for (i=0; i<numapostadores; i++){
+		if(ret<0){
+			perror("Error haciendo fork en el proceso apostador");
+			*tiempo=-1;
+			return -1;
+		}
+		if(ret!=0){
+			while (*tiempo>0){
+				ApuestaMsg apuesta;
+				apuesta.mtype = 1;
+				apuesta.numapostador = rand()%numapostadores;
+				apuesta.numcaballo = rand()%numcaballos;
+				apuesta.apuesta = (double)rand()/RAND_MAX*1000;
+				msgsnd(msqid, (struct msgbuf*) &apuesta, sizeof(ApuestaMsg)-sizeof(long), 0);
+				usleep(100000);
+			}
+			shmdt(tiempo);
+			exit(EXIT_SUCCESS);
+		}
 	}
 }
